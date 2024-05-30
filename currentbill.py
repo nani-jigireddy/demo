30d85e6be1ca8c7de4e396d6dd086232c0db5a7c
nani <jigireddynani264@gmail.com>
2024-05-30 11:28:35 +0530

    add the file

--- currentbill.py
+++ currentbill.py
@@ -1,6 +1,6 @@
 connum=int(input("enter the conusmer number:"))
 custname=input("enter the customer name:") 
-prevreading=int(input("enter the previous reading:"))
+prevreading=int(input("enter the previousgdfehjb reading:"))
 presreading=int(input("enter the present reading:"))
 
 nunits=presreading-prevreading
@@ -13,4 +13,4 @@ print("previous reading:",prevreading)
 print("customer name:",presreading)
 print("no of units:",nunits)
 print("cost per unit:",nunits)
-print("amount:",amnt)     
+print("amount:",amnt)
