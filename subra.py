bb02a3b53090a0c264c4f2e2bdd23bd799e12afb
nani <jigireddynani264@gmail.com>
2024-06-03 15:27:22 +0530

    sub

---
+++ subra.py
@@ -0,0 +1,11 @@
+# Function to subtract two numbers
+def subtract_numbers(num1, num2):
+    result = num1 - num2
+    return result
+ 
+# Example usage
+number1 = 40
+number2 = 18
+subtraction_result = subtract_numbers(number1, number2)
+ 
+print(f"Subtraction using Function: {subtraction_result}
